a78da268f54e3eff4a49f71acac2fe492cfd0e5b
azerty <morozumiyumie@gmail.com>
2022-04-07 14:42:14 +0900

    HighLowGame追加

--- MorozumiMain.java
+++ MorozumiMain.java
@@ -1,10 +1,9 @@
 import java.util.*;
 public class MorozumiMain{
 	public static void main(String [] args){
-		int r = new Random().nextInt(99)+1;
+		int imput, count = 0,r;
+		r = new Random().nextInt(99)+1;
 		System.out.println("1から99の値を一つ生成したよ。いくつかな?>>");
-		int imput;
-		int count = 0;
 		do{
 			count++;
 			imput = new Scanner(System.in).nextInt();
